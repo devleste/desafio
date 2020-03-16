5c4197da96bad1a25e9ad3a6a3d78838ed64a0f5
leomoitinho <leomoitinho@gmail.com>
2020-03-16 03:18:33 -0300

    Its Aliveeeee

---
+++ build/precache-manifest.fed79fd768f6f07e3cddcb500492bfa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "707a7e10a4e9b652d1860c11bfd1b8e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77a5fbd2a4fc1a66ce28",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ad14ac6e353baa56dbb5",
+    "url": "/static/js/2.984e791c.chunk.js"
+  },
+  {
+    "revision": "49974b8d08d9e3074e72a5a7913b6c85",
+    "url": "/static/js/2.984e791c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77a5fbd2a4fc1a66ce28",
+    "url": "/static/js/main.3a788cf0.chunk.js"
+  },
+  {
+    "revision": "568306aef991d128e3a3",
+    "url": "/static/js/runtime-main.9b513cf9.js"
+  }
+]);
