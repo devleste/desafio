788835b017d829338aceea8987325e5364e81c24
andre <andremp1927@hotmail.com>
2020-03-22 13:37:55 -0300

    build criado

---
+++ build/precache-manifest.21f0b280f40aa7333856d3fcfa68f979.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "355d4363d888d6066b3f2d268c6cd846",
+    "url": "/desafio/index.html"
+  },
+  {
+    "revision": "8f48dd81d9abf845a111",
+    "url": "/desafio/static/css/main.f55f5c48.chunk.css"
+  },
+  {
+    "revision": "7ae72fa04a1a2bd3a7c0",
+    "url": "/desafio/static/js/2.b7f84267.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/desafio/static/js/2.b7f84267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f48dd81d9abf845a111",
+    "url": "/desafio/static/js/main.3a3e4028.chunk.js"
+  },
+  {
+    "revision": "515995c764e01247d4dd",
+    "url": "/desafio/static/js/runtime-main.b5319f79.js"
+  },
+  {
+    "revision": "7feeecc732df8ffb8dd4ca2f66df6cd4",
+    "url": "/desafio/static/media/logo.7feeecc7.png"
+  }
+]);
