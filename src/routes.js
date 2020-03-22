7f2899045ceb4607a7943837de56b221f9c18f3f
andre <andremp1927@hotmail.com>
2020-03-22 16:34:54 -0300

    1.2

--- src/routes.js
+++ src/routes.js
@@ -14,14 +14,13 @@ import Footer from './components/footer';
 
 const Routes = () =>{
     return (
-
-        <BrowserRouter>
+    <BrowserRouter>
         <Header/>
             <Switch>
 
-                <Route exact path='/' component={Home}/>
+                <Route path='/' component={Home}/>
                 <Route exact path='/ListaDeContatos' component={ListaDeContatos}/>
-                <Route path='/EditarContatos/:id' component={EditarContatos}/>
+                <Route exact path='/EditarContatos/:id' component={EditarContatos}/>
                 <Route exact path='/AddContatos' component={AddContato}/>
                 <Route exact path='/Estatisticas' component={Estatisticas}/>
                 <Route exact path='/teste' component={Testes}/>
@@ -29,8 +28,7 @@ const Routes = () =>{
 
             </Switch>
         <Footer/>
-
-        </BrowserRouter>
+    </BrowserRouter>
 
     );
 
