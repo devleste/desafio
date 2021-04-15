5e69197d04a95dd4da2ed71986577c7e5ccfbd6c
Luiz Claudio <lcluizclaudio96@gmail.com>
2021-04-15 10:50:12 -0300

    melhorado interface da homepage

--- lesteContact/src/pages/HomePage/index.jsx
+++ lesteContact/src/pages/HomePage/index.jsx
@@ -2,16 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 function HomePage() {
+	const limpaContatos = () => {};
 	return (
 		<Container>
 			<h1>Home Page</h1>
 
 			<ul>
 				<li>
-					<Link to="/contatos">Lista de contatos</Link>
+					<Link to="/contatos">
+						<div>
+							<i class="far fa-address-book"></i>{" "}
+						</div>
+						<strong>Lista de contatos</strong>
+					</Link>
 				</li>
 				<li>
-					<Link to="/contatos/adicionar">Adicionar contatos</Link>
+					<Link to="/contatos/adicionar">
+						<div>
+							<i class="fas fa-user-plus"></i>
+						</div>
+						<strong>Adicionar contatos</strong>
+					</Link>
 				</li>
 			</ul>
 		</Container>
@@ -20,7 +31,42 @@ function HomePage() {
 const Container = styled.div`
 	display: flex;
 	flex-direction: column;
-	justify-content: center;
 	align-items: center;
+	ul {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 90%;
+		padding: 10px;
+		list-style: none;
+		justify-content: flex-start;
+		li {
+			margin: 10px;
+			padding: 10px;
+			width: 90%;
+			justify-content: flex-start;
+			a {
+				display: flex;
+				width: 15em;
+				align-items: center;
+				&:hover {
+					transition: 0.2s;
+					color: #444;
+				}
+			}
+			div {
+				display: flex;
+				height: 2em;
+				width: 2em;
+				background-color: #fff;
+				flex-direction: row;
+				justify-content: center;
+				align-items: center;
+				border-radius: 1em;
+				font-size: 1.5em;
+				margin-right: 10px;
+			}
+		}
+	}
 `;
 export default HomePage;
