ba5e30734a6ef1592a4de69f2becfd6eb78fc57f
stonasss <luis.b.santos96@gmail.com>
2023-10-23 16:29:48 -0300

    config: setting foundations

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+function App() {
+  return (
+    <Screen>
+    </Screen>
+  );
+}
+
+export default App;
+
+const Screen = styled.div`
+  font-family: "sans-serif";
+  background-color: #EDEDED;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
