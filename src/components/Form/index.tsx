8fae1668843681a0277976c788633f0ce9be3a1c
Pedro Cardoso da Silva <pcsilvamaster@gmail.com>
2023-11-23 20:55:34 -0300

    fix: adjusting the form for submission

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -12,7 +12,7 @@ export default function Form(){
   return (
     <section className={style.formSection}>
       <h2>Contact the developer</h2>
-      <form action="https://formsubmit.co/pcsilvamaster@gmail.com" method="POST" className={style.form}>
+      <form action="https://formsubmit.co/2b2c8fbaa26d5ce86bfd7bf3efe4f2d3" method="POST" className={style.form}>
         <Input htmlForName="Name"placeholder="Type your name" name="name" title="Fill in this field" required />
         <Input htmlForName="Email" placeholder="Type your email" title="Fill in this field" name="email" required />
         <div className={style.textareaContainer}>
